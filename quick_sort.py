fdf5efa81a27abae8fbd4105684e7398f0f7e707
Riheng Zhu <43189267+RihengZhu@users.noreply.github.com>
2019-07-17 06:29:27 +0800

    Update quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -15,7 +15,7 @@ def Partition(A,p,r):
     for j in range(p,r):
         if A[j]<=x:
             i+=1
-        A[i],A[j]=A[j],A[i]
+            A[i],A[j]=A[j],A[i]
     A[i+1],A[r]=A[r],A[i+1]
     return i+1
 def median3(A,p,r):
